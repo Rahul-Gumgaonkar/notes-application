6915ce6e73ae8f946e58cd8c081d2af95e12813c
Rahul-Gumgaonkar <rahulgumgaonkar4@gmail.com>
2024-01-07 23:37:00 +0530

    Todo Application

---
+++ src/components/Note.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
+function Note({ note, handleDeleteNotes }) {
+  return (
+    <div className="note">
+      <span>{note.text}</span>
+      <div className="note-footer">
+        <small>{note.date}</small>
+        <div className="delete-icon" onClick={() => handleDeleteNotes(note.id)}>
+          <FaTrashAlt />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Note;
---
+++ src/components/Search.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+
+export default function Search({ handleSearchBar }) {
+  return (
+    <div className="search-container">
+      <FaSearch className="search-icon" />
+      <input
+        onChange={(e) => {
+          handleSearchBar(e.target.value);
+        }}
+        type="text"
+        placeholder="Type to search notes..."
+      />
+    </div>
+  );
+}
